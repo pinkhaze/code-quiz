bbfad9c57a2e984afeda311517bab11f5d4a68d8
Monika <raciam@oregonstate.edu>
2023-05-24 22:53:51 -0500

    logic flow with functions

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+function startQuiz() {
+}
+
+// Load questions on screen
+function renderQuestions() {
+    
+}
+
+function checkAnswers() {
+
+}
+
+function endQuiz() {
+
+}
+
+function setHighScores() {
+
+}
+
+function getHighScores() {
+
+}
+
+function startTimer() {
+
+}
+
+function viewHighScores() {
+
+}
+
+function clearScores() {
+    
+}
+
+function init() {
+
+}
+
