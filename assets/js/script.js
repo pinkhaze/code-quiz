fa8c40c20367076a41c6f71848d5604206b231c2
Monika <raciam@oregonstate.edu>
2023-05-24 22:55:10 -0500

    add variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+var startButton = document.getElementById('start-button');
+var startPage = document.getElementById('start-page');
+var quizContent = document.getElementById('quiz-content');
+var timerElement = document.getElementById("timer-count");
+var endPage = document.getElementById("end-page");
+var scoresPage = document.getElementById("scores-page");
+var submitButton = document.getElementById("submit-button");
+var goBackButton = document.getElementById("go-back-button");
+var clearScoresButton = document.getElementById("clear-scores-button")
+
+var timer;
+var timerCount;
+
 function startQuiz() {
 }
 
