8f91f1ec9e2e4d72b28a24ae6f3a5cdbfc7618b0
Monika <raciam@oregonstate.edu>
2023-10-05 05:12:31 -0500

    Update renderQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,43 +3,78 @@ var startPage = document.getElementById('start-page');
 var quizContent = document.getElementById('quiz-content');
 var timerElement = document.getElementById("timer-count");
 var endPage = document.getElementById("end-page");
-var scoresPage = document.getElementById("scores-page");
-var submitButton = document.getElementById("submit-button");
-var goBackButton = document.getElementById("go-back-button");
-var clearScoresButton = document.getElementById("clear-scores-button")
+var questionElement = document.getElementById('question');
+var answersElement = document.getElementById('answers');
 
 var timer;
-var timerCount;
+var timerCount = 25;
+var index = 0;
+var score = 0;
+
+var questionBank = [
+    {
+        question: "Which one of these is not a JavaScript data type?:",
+        answers: ["Boolean", "String", "Number", 'Float'],
+        answer: "Float",
+    },
+    {
+        question: "How do you declare a JavaScript variable?:",
+        answers: ["num var;", "variable num;", "var num;", 'var=num;'],
+        answer: "var num;",
+      },
+      {
+        question: "Which operator assigns a value to a variable in JavaScript?:",
+        answers: ["!", "=", "+", '/'],
+        answer: "=",
+      },
+      {
+        question: "How do you add a single-line comment in JavaScript:",
+        answers: ["/*Hello", "//Hello", "//Hello//", '<!--Hello-->'],
+        answer: "//Hello",
+      }
+  ];
 
 function startQuiz() {
-    console.log('Testing start button')
+    //console.log('Testing start button')
     startPage.classList.add('hide')
     quizContent.classList.remove('hide')
-    timerCount = 10;
-    renderQuestions()
     startTimer()
+    renderQuestion()
 }
 
-// Load questions on screen
-function renderQuestions() {
+function renderQuestion() {
+    var currQuestion = questionBank[index];
+    for (var i = 0; i < currQuestion.answers.length; i++) {
+        questionElement.textContent = currQuestion.question;
+        var answerButton = document.createElement('button');
+        var currAnswer = currQuestion.answers[i];
+        answerButton.textContent = currAnswer;
+        answerButton.setAttribute('value', currAnswer);
+        answersElement.appendChild(answerButton);
+    }
+}
+
+function checkAnswer(event) {
+    var buttonEl = event.target;
+    if (buttonEl.value === questionBank[index].answer){
+        score += 15;
+    } else {
+        timerCount -= 15;
+    }
     
-}
-
-function checkAnswers() {
-
+    index++;
+    if (index === questionBank.length) {
+        endQuiz()
+    } else {
+        answersElement.innerHTML = '';
+        renderQuestion()
+    }
 }
 
 function endQuiz() {
     quizContent.classList.add('hide');
     endPage.classList.remove('hide');
-}
-
-function setHighScores() {
-
-}
-
-function getHighScores() {
-
+    console.log(score);
 }
 
 function startTimer() {
@@ -54,25 +89,8 @@ function startTimer() {
     }, 1000);
 }
 
-function viewHighScores() {
-    console.log("Testing Submit button");
-    endPage.classList.add("hide");
-    scoresPage.classList.remove("hide");
-
-    
-
-}
-
-function clearScores() {
-    console.log("Testing Clear High Scores button");
-
-}
-
-function init() {
-    
-}
-
 startButton.addEventListener('click', startQuiz);
+answersElement.addEventListener('click', checkAnswer);
 submitButton.addEventListener('click', viewHighScores);
-goBackButton.addEventListener('click', startQuiz);
+goBackButton.addEventListener('click', init);
 clearScoresButton.addEventListener('click', clearScores);
